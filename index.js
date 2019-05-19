ec016ed65d671dcc1b5cfcc74ea62bbb9d3d4cbb
Laura Ojala <laura.ojala@meteoriitti.com>
2019-05-19 07:58:43 +0300

    node server for phone book and 3 GET methods: /info /api/persons /api/persons/:id

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const bodyParser = require('body-parser') 
+const app = express()
+
+let persons = [
+    {
+        name: "Äksy O'Brian",
+        number: "040-123456",
+        id: 1
+    },
+    {
+        name: 'Seke Äksyläinen',
+        number: '050-123123',
+        id: 2
+    },
+    {
+        name: "Karo O'Brian-Karoliininen",
+        number: "060-1234",
+        id: 3
+    },
+    {
+        name: "Sekeliina Kukkonen",
+        number: "070-231",
+        id: 4
+    },
+]
+  
+app.use(bodyParser.json())
+
+app.get('/api/persons', (req, res) => {
+  
+    res.json(persons)
+  })
+
+app.get('/info', (req, res) => {
+    const len = persons.length
+    const date = new Date()
+    res.send(`<div>Phone book has ${len} persons<div>${date}<div>`)
+  })
+
+  app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+  
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
+
+
+  
+
+const PORT = 3001
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`)
+})
